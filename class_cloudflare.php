914bd64dab032df655205b7a74eef3a7333994b1
AzzABTN <azza@broadcasthe.net>
2011-10-05 23:19:01 +1100

    and done for now

--- class_cloudflare.php
+++ class_cloudflare.php
@@ -9,53 +9,53 @@
  */
 class cloudflare_api {
 	
-	//The URL of the API
-	const URL = 'https://www.cloudflare.com/api_json.html';
+	 //The URL of the API
+	 const URL = 'https://www.cloudflare.com/api_json.html';
 	
-	//Timeout for the API requests in seconds
-	const TIMEOUT = 5;
+	 //Timeout for the API requests in seconds
+	 const TIMEOUT = 5;
 	
-	//Stores the api key
-	private $token_key;
+	 //Stores the api key
+	 private $token_key;
 	
-	//Stores the email login
-	private $email;
+	 //Stores the email login
+	 private $email;
 	
-	//Data to post
-	private $data = array;
+	 //Data to post
+	 private $data = array();
 	
-	/**
-	 * Make a new instance of the API client
-	 */
-	public function __construct ($email, $token_key) {
+	 /**
+	  * Make a new instance of the API client
+	  */
+	 public function __construct ($email, $token_key) {
 		$this->email = $email;
 		$this->token_key = $token_key;
-	}
+	 }
 	
-	public function setEmail($email){
+	 public function setEmail($email){
 		$this->email = $email;
-	}
+	 }
 	
-	public function setToken($token_key){
+	 public function setToken($token_key){
 		$this->token_key = $token_key;
-	}
+	 }
 	
-	/**
-	 * Developer Mode - This function allows you to toggle Development Mode on or off for a particular domain. 
-	 * When Development Mode is on the cache is bypassed. Development mode remains on for 3 hours or 
-	 * until when it is toggled back off.
-	 */
-	public function devmode($mode, $domain){
+	 /**
+	  * Developer Mode - This function allows you to toggle Development Mode on or off for a particular domain. 
+	  * When Development Mode is on the cache is bypassed. Development mode remains on for 3 hours or 
+	  * until when it is toggled back off.
+	  */
+	 public function devmode($mode, $domain){
 		$data['a'] = "devmode";
 		$data['z'] = $domain;
 		$data['v'] = ($mode == true) ? 1 : 0;
 		return $this->http_post($data);
-	}
+	 }
 	
-	/**
-	 * Purge Cache - This function will purge CloudFlare of any cached files. It may take up to 48 hours for
-	 * the cache to rebuild and optimum performance to be achieved so this function should be used sparingly.
-	 */
+	 /**
+	  * Purge Cache - This function will purge CloudFlare of any cached files. It may take up to 48 hours for
+	  * the cache to rebuild and optimum performance to be achieved so this function should be used sparingly.
+	  */
 	 public function purge_cache($mode, $domain){
 		$data['a'] = "fpurge_ts";
 		$data['z'] = $domain;
@@ -63,53 +63,53 @@ class cloudflare_api {
 		return $this->http_post($data);
 	 }
 	
-	/**
-	 * You can add an IP address to your whitelist.
-	 */
-	public function whitelist_ip($ip){
+	 /**
+	  * You can add an IP address to your whitelist.
+	  */
+	 public function whitelist_ip($ip){
 		$data['a'] = "wl";
 		$data['key'] = $ip;
 		return $this->http_post($data);
-	}
+	 }
 	
-	/**
-	 * You can add an IP address to your blacklist.
-	 */
-	public function blacklist_ip($ip){
+	 /**
+	  * You can add an IP address to your blacklist.
+	  */
+	 public function blacklist_ip($ip){
 		$data['a'] = "ban";
 		$data['key'] = $ip;
 		return $this->http_post($data);
-	}
+	 }
 	
-	/**
-	 * Set Cache Level - This function sets the Caching Level to Aggressive or Basic. (agg|basic)
-	 */
-	public function set_cache_lvl($mode, $domain){
+	 /**
+	  * Set Cache Level - This function sets the Caching Level to Aggressive or Basic. (agg|basic)
+	  */
+	 public function set_cache_lvl($mode, $domain){
 		$data['a'] = "cache_lvl";
 		$data['z'] = $domain;
 		$data['v'] = ($mode == 'agg') ? 'agg' : 'basic';
 		return $this->http_post($data);
-	}
+	 }
 	
-	/**
-	 * Set Security Level - This function sets the Basic Security Level to HIGH / MEDIUM / LOW / ESSENTIALLY OFF.
-	 * (high|med|low|eoff)
-	 */
-	public function set_security_lvl($mode, $domain){
+	 /**
+	  * Set Security Level - This function sets the Basic Security Level to HIGH / MEDIUM / LOW / ESSENTIALLY OFF.
+	  * (high|med|low|eoff)
+	  */
+	 public function set_security_lvl($mode, $domain){
 		$data['a'] = "sec_lvl";
 		$data['z'] = $domain;
 		$data['v'] = $mode;
 		return $this->http_post($data);
-	}
+	 }
 	
-	/**
-	 * Pull recent IPs hitting your site
-	 * Returns a list of IP addresses which hit your site classified by type.
-	 * $ZoneID = ID of the zone you would like to check. 
-	 * $Hours = Number of hours to go back. Default is 24, max is 48.
-	 * $Class = Restrict the result set to a given class. Currently r|s|t, for regular, crawler, threat resp.
-	 * $Geo = Optional token. Add to add longitude and latitude information to the response. 0,0 means no data.
-	 */
+	 /**
+	  * Pull recent IPs hitting your site
+	  * Returns a list of IP addresses which hit your site classified by type.
+	  * $zoneid = ID of the zone you would like to check. 
+	  * $hours = Number of hours to go back. Default is 24, max is 48.
+	  * $class = Restrict the result set to a given class. Currently r|s|t, for regular, crawler, threat resp.
+	  * $geo = Optional token. Add to add longitude and latitude information to the response. 0,0 means no data.
+	  */
 	 public function get_zone_ips($zoneid, $hours, $class, $geo = '0,0'){
 		$data['a'] = 'zone_ips';
 		$data['zid'] = $zoneid;
@@ -127,52 +127,70 @@ class cloudflare_api {
       * $name = The name of the record you wish to create.
       * $mode = 0 or 1. 0 means CloudFlare is off (grey cloud) for the new zone, while 1 means a happy orange cloud.
 	  */
-	  public function add_dns_record($zone, $type, $content, $name, $mode){
+	 public function add_dns_record($zone, $type, $content, $name, $mode){
 		$data['a'] = 'rec_set';
 		$data['type'] = ($type == 'A') ? 'A' : 'CNAME';
 		$data['content'] = $content;
 		$data['name'] = $name; 
 		$data['service_mode'] = ($mode == true) ? 1 : 0;
 		return $this->http_post($data);
-	  }
+	 }
 	  
-	  /**
-	   * Update an existing DNS record - Update a DNS record for your site. This needs to be an A record.
-	   * $ip = The value of the IP address (the destination).
-       * $hosts = The name of the record you wish to create.
-	   */
-	  public function update_dns_record($ip, $hosts){
+	 /**
+	  * Update an existing DNS record - Update a DNS record for your site. This needs to be an A record.
+	  * $ip = The value of the IP address (the destination).
+	  * $hosts = The name of the record you wish to create.
+	  */
+	 public function update_dns_record($ip, $hosts){
 		$data['a'] = "DIUP";
 		$data['ip'] = $ip;
 		$data['hosts'] = $hosts;
 		return $this->http_post($data);
+	 }
+	  
+	 /**
+	  * Toggle IPv6 support for your site - Toggles ipv6 support for a site.
+	  */
+	 public function toggle_ipv6($zone, $mode){
+		$data['a'] = 'ipv46';
+		$data['z'] = $zone;
+		$data['v'] = ($mode == true) ? 1 : 0;
+		return $this->http_post($data);
+	 }
+	 
+	 /**
+	  * Update the snapshot of your site for CloudFlare's challenge page
+	  * Tells CloudFlare to take a new image of your site.
+	  * Note that this call is rate limited to once per zone per day. Also the new image may take up to 1 hour to appear.
+	  */
+	  public function update_image($zoneid){
+		$data['a'] = 'zone_grab';
+		$data['zid'] = $zoneid;
+		return $this->http_post($data);
 	  }
 	
-	/**
-	 * HTTP POST a specific task with the supplied data
-	 */
-	private function http_post ($data) {
-         $data['u'] = $this->email;
-         $data['tkn'] = $this->token_key;
-	     $ch = curl_init();
-	     curl_setopt($ch, CURLOPT_VERBOSE, 0);
-	     curl_setopt($ch, CURLOPT_FORBID_REUSE, true); 
-	     curl_setopt($ch, CURLOPT_URL, self::URL);
-	     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-	     curl_setopt($ch, CURLOPT_POST, 1);
-	     curl_setopt($ch, CURLOPT_POSTFIELDS, $data ); 
-	     curl_setopt($ch, CURLOPT_TIMEOUT, self::TIMEOUT);
-	     $http_result = curl_exec($ch);
-	     $error = curl_error($ch);
-	     $http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
-	     curl_close($ch);
-	   
+	 /**
+	  * HTTP POST a specific task with the supplied data
+	  */
+	 private function http_post ($data) {
+		 $data['u'] = $this->email;
+		 $data['tkn'] = $this->token_key;
+		 $ch = curl_init();
+		 curl_setopt($ch, CURLOPT_VERBOSE, 0);
+		 curl_setopt($ch, CURLOPT_FORBID_REUSE, true); 
+		 curl_setopt($ch, CURLOPT_URL, self::URL);
+		 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		 curl_setopt($ch, CURLOPT_POST, 1);
+		 curl_setopt($ch, CURLOPT_POSTFIELDS, $data ); 
+		 curl_setopt($ch, CURLOPT_TIMEOUT, self::TIMEOUT);
+		 $http_result = curl_exec($ch);
+		 $error = curl_error($ch);
+		 $http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
+		 curl_close($ch);
 	     if ($http_code != 200) {
 		     return array("error"=>$error);
 	     } else {
 		     return json_decode($http_result);
 	     }
-	}
+	 }
 }
-
-
